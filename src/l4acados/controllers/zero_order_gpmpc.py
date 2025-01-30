8a6d21c1d88426235f9ca0170142cf3201d4014a
GregorGartner <115502010+GregorGartner@users.noreply.github.com>
2025-01-30 13:34:41 +0100

    back to original controller

--- src/l4acados/controllers/zero_order_gpmpc.py
+++ src/l4acados/controllers/zero_order_gpmpc.py
@@ -105,8 +105,8 @@ class ZeroOrderGPMPC(ResidualLearningMPC):
             (
                 self.zoro_input_P0,
                 self.zoro_input_W_diag,
-                # self.residual_model.current_variance is updated with value_and_jacobian() call in preparation phase
-                # self.residual_model.current_variance.flatten(),
+                self.residual_model.current_variance is updated with value_and_jacobian() call in preparation phase
+                self.residual_model.current_variance.flatten(),
             )
         )
         covariances_in_len = covariances_in.size
